3b097cdfd58d28575b58036cd017b0f94e2e1243
wenyu <wenyu7980@163.com>
2021-03-26 21:19:23 +0800

    aggregation

--- wy-authentication-service/src/main/java/com/wenyu7980/authentication/role/admin/controller/RoleAdminQueryController.java
+++ wy-authentication-service/src/main/java/com/wenyu7980/authentication/role/admin/controller/RoleAdminQueryController.java
@@ -1,7 +1,6 @@
 package com.wenyu7980.authentication.role.admin.controller;
 
 import com.wenyu7980.authentication.role.admin.domain.RoleAdminListDetail;
-import com.wenyu7980.organization.aggregation.UserAggregation;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,12 +22,6 @@ public class RoleAdminQueryController {
     @GetMapping()
     public List<RoleAdminListDetail> getPage() {
         List<RoleAdminListDetail> details = new ArrayList<>();
-        RoleAdminListDetail detail = new RoleAdminListDetail();
-        detail.setCreatedUser(new UserAggregation("q23"));
-        details.add(detail);
-        RoleAdminListDetail detail1 = new RoleAdminListDetail();
-        detail1.setCreatedUser(new UserAggregation("q234"));
-        details.add(detail1);
         return details;
     }
 }
